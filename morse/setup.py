2daec9a466c57b757711ec0d5a419225b5adcc85
Elvis Pfutzenreuter <epx@mog.com>
2010-12-16 22:43:12 -0200

    Added CoreAudio framework link flag to module

--- morse/setup.py
+++ morse/setup.py
@@ -4,6 +4,7 @@ from Cython.Distutils import build_ext
 
 setup(
         cmdclass = {'build_ext': build_ext},
-        ext_modules = [Extension("coreaudio", ["coreaudio.pyx"])]
+        ext_modules = [Extension("coreaudio", ["coreaudio.pyx"],
+			extra_link_args=['-framework', 'CoreAudio'])]
 )
 
